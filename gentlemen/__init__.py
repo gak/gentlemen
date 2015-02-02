26a4bd535b7431d7abe86914a2260ee448912d98
Gerald Kaszuba <gak@gak0.com>
2015-02-02 23:26:27 +1100

    Neater gentlemen.

--- gentlemen/__init__.py
+++ gentlemen/__init__.py
@@ -3,7 +3,11 @@
 import random
 
 def main():
-    this_is = ["gentlemen", "actually good news"]
+    this_is = [
+        "gentlemen",
+        "actually good news",
+    ]
+
     print('This is {}.'.format(random.choice(this_is)))
 
 if __name__ == '__main__':
